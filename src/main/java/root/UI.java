1793d24bd6b7178d11c4f164c15ceade93f4053b
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-03-29 23:44:08 +0200

    panels added (level - 2)

--- src/main/java/root/UI.java
+++ src/main/java/root/UI.java
@@ -8,15 +8,18 @@ public class UI {
     JFrame window;
     Container con;
 
-    JPanel optionsPanel, mainPanel;
+    JPanel optionsPanel, mainPanel, wordPanel, userPanel;
+    JPanel dataBasePanel, grammarPanel, commentPanel, examplePanel, tagsPanel, picturePanel;
 
     int windowX = 1600;
     int windowY = 900;
 
-    int optionsPanelWidth, mainPanelWidth;
+    int optionsPanelWidth, mainPanelWidth, wordPanelWidth, userPanelWidth;
+    int wordPanelHeight, userPanelHeight;
 
     Color darkGray = new Color(30, 30, 30);
     Color mediumGray = new Color (50, 50, 50);
+    Color lightGray = new Color (80, 80, 80);
     Color mediumPurple = new Color (70, 70, 206);
 
     public UI(){
@@ -36,13 +39,29 @@ public class UI {
 
     void displayScreen() {
         optionsPanelWidth = 300;
-        optionsPanel = makePanel(10, 10, optionsPanelWidth, windowY - 20, mediumGray);
+        optionsPanel = makePanel(5, 5, optionsPanelWidth, windowY - 10, mediumGray);
         con.add(optionsPanel);
 
-        mainPanelWidth = windowX - optionsPanelWidth - 30;
-        mainPanel = makePanel(20 + optionsPanelWidth, 10, mainPanelWidth,
-                windowY - 20, mediumGray);
+        mainPanelWidth = windowX - optionsPanelWidth - 15;
+        mainPanel = makePanel(10 + optionsPanelWidth, 5, mainPanelWidth,
+                windowY - 10, mediumGray);
+        mainPanel.setLayout(null);
         con.add(mainPanel);
+//        mainPanel.setVisible(false);
+
+        wordPanelWidth = mainPanelWidth - 10;
+        wordPanelHeight = 500;
+        wordPanel = makePanel(5, 5, wordPanelWidth, wordPanelHeight, lightGray);
+//        wordPanel.setPreferredSize(new Dimension(wordPanelWidth, wordPanelHeight));
+        mainPanel.add(wordPanel);
+
+        userPanelWidth = wordPanelWidth;
+        userPanelHeight = windowY - wordPanelHeight - 25;
+        userPanel = makePanel(5,wordPanelHeight + 10,
+                userPanelWidth, userPanelHeight, lightGray);
+        mainPanel.add(userPanel);
+
+
 
 
     }
@@ -50,7 +69,9 @@ public class UI {
     JPanel makePanel(int x, int y, int width, int height, Color color) {
         JPanel panel = new JPanel();
         panel.setBounds(x, y, width, height);
+        panel.setPreferredSize(new Dimension(width, height));
         panel.setBackground(color);
+        panel.setVisible(true);
         return panel;
     }
 }
