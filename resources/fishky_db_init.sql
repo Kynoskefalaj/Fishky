03c1d283408839b3abe0438a52cc050636d2d7a0
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-04-24 23:53:53 +0200

    Table: explanation created

--- resources/fishky_db_init.sql
+++ resources/fishky_db_init.sql
@@ -4,14 +4,15 @@ DROP TABLE words;
 
 CREATE TABLE fishky.words
 (
-	word_id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     eng_word VARCHAR(45) NOT NULL,
 	pol_word VARCHAR (225),
     part_of_speech VARCHAR(45)
 );
 -- nie wskazuję silnika składowania, więc podstawi się domyślny (innoDB)
 
-LOAD DATA INFILE 'C:\\Users\\adamg\\Desktop\\Java_learning\\Fishky\\resources\\fishky_db_words.csv'
+LOAD DATA INFILE 
+'C:\\Users\\adamg\\Desktop\\Java_learning\\Fishky\\resources\\fishky_db_words.csv'
 INTO TABLE fishky.words
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -27,3 +28,35 @@ INSERT INTO fishky.words (eng_word, pol_word, part_of_speech)
 VALUES ('test', 'test', 'test');
 
 DELETE FROM fishky.words WHERE word_id = 998;
+
+-- ----------------------------------------------------------------------------------------------
+
+CREATE TABLE fishky.explanation
+(
+	word_id INT NOT NULL UNIQUE,
+    eng_phrase VARCHAR(495),
+    pol_phrase VARCHAR(495),
+    eng_sentence VARCHAR(495),
+    eng_explanation VARCHAR(4050)
+);
+
+LOAD DATA INFILE 
+'C:\\Users\\adamg\\Desktop\\Java_learning\\Fishky\\resources\\fishky_db_explanation.csv'
+INTO TABLE fishky.explanation
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(word_id, @var1, @var2, @var3, @var4)
+SET eng_phrase = NULLIF(@var1, ''), 
+	pol_phrase = NULLIF(@var2, ''), 
+    eng_sentence = NULLIF(@var3, ''), 
+    eng_explanation = NULLIF(@var4, '');
+
+
+SELECT * FROM explanation;
+
+INSERT INTO explanation (word_id, eng_phrase)
+VALUES (997, 'test');
+
+DROP TABLE explanation;
+    
