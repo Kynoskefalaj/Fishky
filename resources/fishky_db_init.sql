4dea3374c9c79c5ef2a089b43049ebcea1fedec2
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-04-25 00:32:55 +0200

    Table: frequency ~created

--- resources/fishky_db_init.sql
+++ resources/fishky_db_init.sql
@@ -14,6 +14,7 @@ CREATE TABLE fishky.words
 LOAD DATA INFILE 
 'C:\\Users\\adamg\\Desktop\\Java_learning\\Fishky\\resources\\fishky_db_words.csv'
 INTO TABLE fishky.words
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES
@@ -22,12 +23,12 @@ IGNORE 1 LINES
 -- naprawa buga, gdzie kolejne nadawane id przy autoinkrementacji wynosi 1024
 ALTER TABLE fishky.words AUTO_INCREMENT = 998;
 
-SELECT * FROM words;
-
 INSERT INTO fishky.words (eng_word, pol_word, part_of_speech)
 VALUES ('test', 'test', 'test');
 
-DELETE FROM fishky.words WHERE word_id = 998;
+SELECT * FROM words;
+
+DELETE FROM fishky.words WHERE id = 998;
 
 -- ----------------------------------------------------------------------------------------------
 
@@ -46,6 +47,7 @@ CREATE TABLE fishky.explanation
 LOAD DATA INFILE 
 'C:\\Users\\adamg\\Desktop\\Java_learning\\Fishky\\resources\\fishky_db_explanation.csv'
 INTO TABLE fishky.explanation
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES
@@ -62,4 +64,37 @@ INSERT INTO explanation (word_id, eng_phrase)
 VALUES (997, 'test');
 
 DROP TABLE explanation;
-    
+
+-- ----------------------------------------------------------------------------------------------
+
+CREATE TABLE fishky.frequency
+(
+	word_id INT NOT NULL UNIQUE,
+    ranking_position INT,
+    frequency_daily INT,
+	frequency_IT INT,
+    frequency_engineering INT,
+    PRIMARY KEY (word_id),
+    CONSTRAINT FK_word_frequency FOREIGN KEY (word_id)
+    REFERENCES fishky.words(id)
+);
+
+LOAD DATA INFILE 
+'C:\\Users\\adamg\\Desktop\\Java_learning\\Fishky\\resources\\fishky_db_frequency.csv'
+INTO TABLE fishky.frequency
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(word_id, @var1, @var2, @var3, @var4)
+SET ranking_position = NULLIF(@var1, ''), 
+	frequency_daily = NULLIF(@var2, ''), 
+    frequency_IT = NULLIF(@var3, ''), 
+    frequency_engineering = NULLIF(@var4, '');
+
+
+SELECT * FROM frequency;
+
+INSERT INTO frequency (word_id, ranking_position)
+VALUES (997, 'test');
+
+DROP TABLE frequency;
