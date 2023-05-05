d1b8b7b23e20e23e35eff11c295d57287fd03c89
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-05-05 23:46:50 +0200

    queryTest

--- src/test/java/DatabaseConnection.java
+++ src/test/java/DatabaseConnection.java
@@ -2,12 +2,25 @@ import org.junit.Test;
 import root.DbConnector;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
 
     @Test
-    public static void main(String[] args) throws SQLException {
+    public void connectionTest() throws SQLException {
         Connection connection = DbConnector.connect();
+    }
+
+    @Test
+    public void queryTest() throws SQLException {
+
+        try {
+            ResultSet result = QueryExecutor.executeSelect("SELECT * FROM words WHERE id = 666");
+            String wordName = result.getString("eng_word");
+            System.out.println(wordName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
